289bab34e618345f34971dc681b4054ca0afc28d
Michael Gruner <michael.gruner@ridgerun.com>
2015-09-21 23:41:27 -0600

    Adding file to handle elements

---
+++ gstd/gstd_element.c
@@ -0,0 +1,36 @@
+/*
+ * Gstreamer Daemon - Gst Launch under steroids
+ * Copyright (C) 2015 RidgeRun Engineering <support@ridgerun.com>
+ *
+ * This file is part of Gstd.
+ *
+ * Gstd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gstd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Gstd.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "gstd_element.h"
+
+GstdReturnCode
+gstd_element_get_list (GstdPipeline *pipeline, GList **elements)
+{
+  g_return_val_if_fail(pipeline, GSTD_MISSING_INITIALIZATION);
+
+  /* If this is not null, we want to advice the user a potential memory leak */
+  g_warn_if_fail(!*elements);
+
+  GST_INFO("Returning elements in pipeline \"%s\"",
+	   GSTD_PIPELINE_NAME(pipeline));
+  
+  *elements = GST_BIN_CHILDREN(GSTD_PIPELINE_PIPELINE(pipeline));
+
+  return GSTD_EOK;
+}
---
+++ gstd/gstd_element.h
@@ -0,0 +1,41 @@
+/*
+ * Gstreamer Daemon - Gst Launch under steroids
+ * Copyright (C) 2015 RidgeRun Engineering <support@ridgerun.com>
+ *
+ * This file is part of Gstd.
+ *
+ * Gstd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gstd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Gstd.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GSTD_ELEMENT_H__
+#define __GSTD_ELEMENT_H__
+
+#include <gst/gst.h>
+#include "gstd_return_codes.h"
+#include "gstd_debug.h"
+#include "gstd_pipeline.h"
+
+/**
+ * Returns a list of the elements contained in the given GstdPipeline
+ *
+ * \param pipeline A valid GstdPipeline created by using gstd_pipeline_create
+ * \param elements A double pointer to a GList to hold the element list. Must
+ * be NULL. Do not free this list!
+ *
+ * \return The appropriate return code.
+ */
+GstdReturnCode
+gstd_element_get_list (GstdPipeline *pipeline, GList **elements);
+
+#endif // __GSTD_ELEMENT_H__
