872150d8f36b033ae6a4d60a8706087bcab228ea
Juan Jose Valerio Zamora <juan.valerio@ridgerun.com>
2017-02-02 14:01:58 -0600

    Cleanup tests.

--- tests/gstd/test_gstd_session.c
+++ tests/gstd/test_gstd_session.c
@@ -26,20 +26,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void singleton_instantiation_test(){
   GstdSession* temp1 = NULL;
   GstdSession* temp2 = NULL;
-  temp1 = gstd_session_new ("Session0", 8002);
+  gint port1 = 1;
+  gint port2 = 2;
+  temp1 = gstd_session_new ("Session0", 8001);
   g_assert_true (temp1 != temp2);
   temp2 = gstd_session_new ("Session0", 8002);
   g_assert_true (temp1 == temp2);
+  g_object_get(temp1, "port", &port1, NULL);
+  g_object_get(temp2, "port", &port2, NULL);
+  g_print("GstdSession temp1 ptr: %p, GstdSession temp2 ptr: %p\n", temp1, temp2);
+  g_print("port 1: %d port 2: %d  \n", port1, port2);
+  g_assert_cmpint(port1, == , 8001);
+  g_assert_cmpint(port1, == , port2);
   g_object_unref(temp1);
   g_object_unref(temp2);
 }
 
 void session_mem_leak_test(){
-    gint reps = 100;
+    gint reps = 10;
     gint i;
     for (i = 0; i < reps; ++i){
         singleton_instantiation_test();
@@ -59,30 +68,34 @@ void* instantiate_session_singleton(gpointer address){
 void thread_safety_instantiation_test()
 {
 
-  gint reps = 100;
-  gint num_threads = 5;
+  gint reps = 10;
+  gint num_threads = 3;
   gint i, j, r;
   GThread *threads[num_threads];
   GstdSession *sessions[num_threads];
 
   //We need at least 2 threads to test
-  g_assert_true(num_threads > 0);
+  g_assert_true(num_threads > 2);
 
   for (r = 0; r < reps; ++r)
   {
+    //spawn threads
     for (i = 0; i < num_threads; ++i)
     {
       threads[i] = g_thread_new("thread", instantiate_session_singleton, (gpointer)&sessions[i]);
-      //Check all singletons are the same
     }
 
+    //wait for threads to finish
     g_thread_join(threads[0]);
     for (j = 1; j < num_threads; ++j)
     {
       g_thread_join(threads[j]);
       //Check all singletons are the same
-      g_assert_true(sessions[j] == sessions[j - 1]);
+      g_print("GstdSession ptr %d: %p, GstdSession ptr %d: %p \n", j-1, sessions[j-1], j, sessions[j]);
+      g_assert_true(sessions[j-1] == sessions[j]);
+      g_object_unref(sessions[j-1]);
     }
+    g_object_unref(sessions[num_threads-1]);
   }
 }
 
