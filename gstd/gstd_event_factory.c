c0d8d7d7197548636d76c6d0286119f8ce7d1127
Juan Jose Valerio Zamora <juan.valerio@ridgerun.com>
2017-02-22 18:09:35 -0600

    Parse arguments for seek event.

--- gstd/gstd_event_factory.c
+++ gstd/gstd_event_factory.c
@@ -75,11 +75,29 @@ enum _GstdEventType
 };
 
 GstdEventType gstd_event_factory_parse_event (gchar *);
+static GstEvent* gstd_event_factory_make_seek_event(gchar *);
 
 GstEvent * gstd_event_factory_make (gchar *name, gchar *description){
 
   GstEvent *event = NULL;
   GstdEventType type = gstd_event_factory_parse_event(name);
+  
+  switch(type){
+  case GSTD_EVENT_EOS:
+    event = gst_event_new_eos ();
+    break; 
+  case GSTD_EVENT_SEEK:
+    event = gstd_event_factory_make_seek_event (description);
+    break;
+  default:
+    break;
+  }
+  
+  return event;
+}
+
+static GstEvent* gstd_event_factory_make_seek_event(gchar *description)
+{
   gdouble rate = GSTD_EVENT_FACTORY_SEEK_RATE_DEFAULT;
   GstFormat format = GSTD_EVENT_FACTORY_SEEK_FORMAT_DEFAULT;
   GstSeekFlags flags = GSTD_EVENT_FACTORY_SEEK_FLAGS_DEFAULT;
@@ -87,19 +105,46 @@ GstEvent * gstd_event_factory_make (gchar *name, gchar *description){
   gint64 start = GSTD_EVENT_FACTORY_SEEK_START_DEFAULT;
   GstSeekType stop_type = GSTD_EVENT_FACTORY_SEEK_STOP_TYPE_DEFAULT;
   gint64 stop = GSTD_EVENT_FACTORY_SEEK_STOP_DEFAULT;
-  
-  switch(type){
-  case GSTD_EVENT_EOS:
-    event = gst_event_new_eos ();
-    break; 
-  case GSTD_EVENT_SEEK:
-    event = gst_event_new_seek (rate,format,flags,start_type,start,stop_type,stop);
-    break;
-  default:
-    break;
+
+  //Assume all 7 properties come with at most one value
+  gchar **tokens = g_strsplit (description, " ", 14);
+
+  if (strncmp(tokens[0],"rate",6)){
+    return NULL;
   }
-  
-  return event;
+  rate = g_ascii_strtod (tokens[1], NULL);
+
+  if (strncmp(tokens[2],"format",8)){
+    return NULL;
+  }
+  format = g_ascii_strtoll(tokens[3], NULL, 0);
+
+  if (strncmp(tokens[4],"flags",7)){
+    return NULL;
+  }
+  flags = g_ascii_strtoll(tokens[5], NULL, 0);
+
+  if (strncmp(tokens[6],"start_type",12)){
+    return NULL;
+  }
+  start_type = g_ascii_strtoll(tokens[7], NULL, 0);
+
+  if (strncmp(tokens[8],"start",7)){
+    return NULL;
+  }
+  start = g_ascii_strtoll(tokens[9], NULL, 0);
+
+  if (strncmp(tokens[10],"stop_type",11)){
+    return NULL;
+  }
+  stop_type = g_ascii_strtoll(tokens[11], NULL, 0);
+
+  if (strncmp(tokens[12],"stop",6)){
+    return NULL;
+  }
+  stop = g_ascii_strtoll(tokens[13], NULL, 0);
+
+  return gst_event_new_seek (rate,format,flags,start_type,start,stop_type,stop);
 }
 
 GstdEventType 
