3b78dd3f289a14b9a39c38896e25182b11e061fc
Michael Gruner <michael.gruner@ridgerun.com>
2017-04-26 21:18:19 -0600

    Avoid failure on null resource read
    In some cases it is valid to fetch a resource that turns out to be
    null. For example, when reading a bus message, the timeout may expire
    and, hence, no object will be returned. In this cases we just return
    null to the user instead of failing.

--- gstd/gstd_tcp.c
+++ gstd/gstd_tcp.c
@@ -514,8 +514,8 @@ gstd_tcp_parse_raw_cmd (GstdSession * session, gchar * action, gchar * args,
     uri = "/";
 
   ret = gstd_get_by_uri (session, uri, &node);
-  if (ret) {
-    goto baduri;
+  if (ret || NULL == node) {
+    goto out;
   }
 
   if (!g_ascii_strcasecmp ("CREATE", action)) {
@@ -533,7 +533,7 @@ gstd_tcp_parse_raw_cmd (GstdSession * session, gchar * action, gchar * args,
 
   g_object_unref (node);
 
-baduri:
+out:
   {
     g_strfreev (tokens);
     return ret;
