a8f3d9cc342c069d1d0187d9f960b2d117ace8ba
Michael Gruner <michael.gruner@ridgerun.com>
2017-04-26 23:09:34 -0600

    Remove assertion on phony property getter

--- gstd/gstd_pipeline_bus.c
+++ gstd/gstd_pipeline_bus.c
@@ -91,7 +91,6 @@ gstd_pipeline_bus_class_init (GstdPipelineBusClass * klass)
 
   g_object_class_install_properties (object_class, N_PROPERTIES, properties);
 
-
   /* Initialize debug category with nice colors */
   guint debug_color = GST_DEBUG_FG_BLACK | GST_DEBUG_BOLD | GST_DEBUG_BG_WHITE;
   GST_DEBUG_CATEGORY_INIT (gstd_pipeline_bus_debug, "gstdpipelinebus",
@@ -152,7 +151,6 @@ gstd_pipeline_bus_get_property (GObject * object,
       // FIXME: Serialize the message by overriding to_string(), this is not a real
       // property, is just me being lazy
       g_value_set_object (value, NULL);
-      g_return_if_reached();
       break;
     case PROP_TIMEOUT:
       GST_DEBUG_OBJECT (self, "Returning timeout %" GST_TIME_FORMAT, GST_TIME_ARGS(self->timeout));
@@ -184,3 +182,4 @@ gstd_pipeline_bus_get_bus (GstdPipelineBus *self)
 
   return gst_object_ref (self->bus);
 }
+
