354dfbe86ef5110e7c67881b0c40563d56af7f2a
Jennifer <jennifer.caballero@ridgerun.com>
2017-05-22 12:09:07 -0600

    Extend create tests to check pipeline creation without description

--- tests/gstd/test_gstd_create.c
+++ tests/gstd/test_gstd_create.c
@@ -44,6 +44,25 @@ GST_START_TEST (test_create_no_name)
 }
 GST_END_TEST;
 
+
+GST_START_TEST (test_create_no_description)
+{
+  GstdObject *node;
+  GstdReturnCode ret;
+  GstdSession *test_session = gstd_session_new ("Test_session");
+
+  ret = gstd_get_by_uri (test_session, "/pipelines", &node);
+  fail_if (ret);
+  fail_if (NULL == node);
+
+  ret = gstd_object_create (node, "p2", NULL);
+  fail_if (GSTD_MISSING_ARGUMENT != ret);
+
+  gst_object_unref(node);
+  gst_object_unref(test_session);
+}
+GST_END_TEST;
+
 static Suite *
 gstd_create_suite (void)
 {
@@ -53,6 +72,7 @@ gstd_create_suite (void)
   suite_add_tcase (suite, tc);
   tcase_add_test (tc, test_successful_create);
   tcase_add_test (tc, test_create_no_name);
+  tcase_add_test (tc, test_create_no_description);
 
   return suite;
 }
