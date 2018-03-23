346cfbc5f4a60b2dd9715bb1d75af68b13360d76
Michael Gruner <michael.gruner@ridgerun.com>
2018-03-23 17:57:12 -0600

    Serialize stream status bus message

--- gstd/gstd_bus_msg_stream_status.c
+++ gstd/gstd_bus_msg_stream_status.c
@@ -34,6 +34,9 @@ static GstdReturnCode
 gstd_bus_msg_stream_status_to_string (GstdBusMsg * msg, GstdIFormatter * formatter,
     GstMessage * target);
 
+static const gchar *
+gstd_bus_msg_stream_status_code_to_string (GstStreamStatusType type);
+
 struct _GstdBusMsgStreamStatus
 {
   GstdBusMsg parent;
@@ -68,13 +71,76 @@ gstd_bus_msg_stream_status_init (GstdBusMsgStreamStatus * self)
   GST_INFO_OBJECT (self, "Initializing bus Stream Status message");
 }
 
+static const gchar *
+gstd_bus_msg_stream_status_code_to_string (GstStreamStatusType type)
+{
+  const gchar * type_name;
+
+  switch (type) {
+  case GST_STREAM_STATUS_TYPE_CREATE:
+    type_name = "GST_STREAM_STATUS_TYPE_CREATE";
+    break;
+  case GST_STREAM_STATUS_TYPE_ENTER:
+    type_name = "GST_STREAM_STATUS_TYPE_ENTER";
+    break;
+  case GST_STREAM_STATUS_TYPE_LEAVE:
+    type_name = "GST_STREAM_STATUS_TYPE_LEAVE";
+    break;
+  case GST_STREAM_STATUS_TYPE_DESTROY:
+    type_name = "GST_STREAM_STATUS_TYPE_DESTROY";
+    break;
+  case GST_STREAM_STATUS_TYPE_START:
+    type_name = "GST_STREAM_STATUS_TYPE_START";
+    break;
+  case GST_STREAM_STATUS_TYPE_PAUSE:
+    type_name = "GST_STREAM_STATUS_TYPE_PAUSE";
+    break;
+  case GST_STREAM_STATUS_TYPE_STOP:
+    type_name = "GST_STREAM_STATUS_TYPE_STOP";
+    break;
+  default:
+    type_name = "UNKNOWN";
+    break;
+  }
+
+  return type_name;
+}
+
 static GstdReturnCode
 gstd_bus_msg_stream_status_to_string (GstdBusMsg * msg, GstdIFormatter * formatter,
     GstMessage * target)
 {
+  GstStreamStatusType type;
+  GValue value = G_VALUE_INIT;
+  GstElement * owner;
+  GstElementFactory * factory;
+
   g_return_val_if_fail (msg, GSTD_NULL_ARGUMENT);
   g_return_val_if_fail (formatter, GSTD_NULL_ARGUMENT);
   g_return_val_if_fail (target, GSTD_NULL_ARGUMENT);
+
+  gst_message_parse_stream_status (target, &type, &owner);
+
+  gstd_iformatter_set_member_name (formatter, "stream_status");
+  gstd_iformatter_begin_object (formatter);
+
+  gstd_iformatter_set_member_name (formatter, "type");
+  g_value_init (&value, G_TYPE_INT);
+  g_value_set_int (&value, type);
+  gstd_iformatter_set_value (formatter, &value);
+  g_value_unset (&value);
+
+  gstd_iformatter_set_member_name (formatter, "type_name");
+  gstd_iformatter_set_string_value (formatter, gstd_bus_msg_stream_status_code_to_string (type));
+
+  gstd_iformatter_set_member_name (formatter, "owner");
+  gstd_iformatter_set_string_value (formatter, GST_OBJECT_NAME(owner));
+
+  gstd_iformatter_set_member_name (formatter, "owner_factory");
+  factory = gst_element_get_factory (owner);
+  gstd_iformatter_set_string_value (formatter, GST_OBJECT_NAME(factory));
+
+  gstd_iformatter_end_object (formatter);
 
   return GSTD_EOK;
 }
