a718e46981075ed10ea07718a6c02eec9626efa2
Michael Gruner <michael.gruner@ridgerun.com>
2018-05-17 18:32:56 -0600

    Implement integer parsing from JSON

---
+++ libgstc/libgstc_json.c
@@ -0,0 +1,58 @@
+/*
+ * GStreamer Daemon - gst-launch on steroids
+ * C client library abstracting gstd interprocess communication
+ *
+ * Copyright (c) 2015-2018 RidgeRun, LLC (http://www.ridgerun.com)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <jansson.h>
+#include <stdlib.h>
+
+#include "libgstc_assert.h"
+#include "libgstc_json.h"
+
+GstcStatus
+gstc_json_get_int (const char *json, const char *name, int *out)
+{
+  json_t *root;
+  json_t *data;
+  json_error_t error;
+
+  gstc_assert_and_ret_val (json != NULL, GSTC_NULL_ARGUMENT);
+  gstc_assert_and_ret_val (name != NULL, GSTC_NULL_ARGUMENT);
+  gstc_assert_and_ret_val (out != NULL, GSTC_NULL_ARGUMENT);
+
+  root = json_loads (json, 0, &error);
+  data = json_object_get (root, name);
+
+  *out = json_integer_value (data);
+
+  json_decref (root);
+
+  return GSTC_OK;
+}
---
+++ tests/libgstc/test_libgstc_json.c
@@ -0,0 +1,81 @@
+/*
+ * GStreamer Daemon - Gst Launch under steroids
+ * Copyright (c) 2015-2018 Ridgerun, LLC (http://www.ridgerun.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+#include <gst/check/gstcheck.h>
+
+#include "libgstc_json.h"
+
+/* Test Fixture */
+
+
+GST_START_TEST (test_json_empty)
+{
+  GstcStatus ret;
+  gint code;
+
+  ret = gstc_json_get_int (NULL, "code", &code);
+
+  assert_equals_int (GSTC_NULL_ARGUMENT, ret);
+}
+
+GST_END_TEST;
+
+GST_START_TEST (test_json_int)
+{
+  GstcStatus ret;
+  gint code;
+  const char *json = "{ \"string\" : \"string val\", \"int\" : 100 }";
+
+  ret = gstc_json_get_int (json, "int", &code);
+
+  assert_equals_int (GSTC_OK, ret);
+  assert_equals_int (code, 100);
+}
+
+GST_END_TEST;
+
+GST_START_TEST (test_json_int_neg)
+{
+  GstcStatus ret;
+  gint code;
+  const char *json = "{ \"string\" : \"string val\", \"int\" : -100 }";
+
+  ret = gstc_json_get_int (json, "int", &code);
+
+  assert_equals_int (GSTC_OK, ret);
+  assert_equals_int (code, -100);
+}
+
+GST_END_TEST;
+
+static Suite *
+libgstc_ping_suite (void)
+{
+  Suite *suite = suite_create ("libgstc_json");
+  TCase *tc = tcase_create ("general");
+
+  suite_add_tcase (suite, tc);
+
+  tcase_add_test (tc, test_json_empty);
+  tcase_add_test (tc, test_json_int);
+  tcase_add_test (tc, test_json_int_neg);
+
+  return suite;
+}
+
+GST_CHECK_MAIN (libgstc_ping);
