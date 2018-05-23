dfbb298c25ec155e4d167f716e0fee3932b845a0
Michael Gruner <michael.gruner@ridgerun.com>
2018-05-22 22:04:09 -0600

    Implement socket module

--- libgstc/libgstc_socket.c
+++ libgstc/libgstc_socket.c
@@ -30,30 +30,90 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <arpa/inet.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
+#include "libgstc_assert.h"
 #include "libgstc_socket.h"
+
+/* Allow the user to override this value at build time */
+#ifndef GSTC_MAX_RESPONSE_LENGTH
+#  define GSTC_MAX_RESPONSE_LENGTH 4096
+#endif
 
 struct _GstcSocket
 {
+  int socket;
+  struct sockaddr_in server;
 };
 
 GstcSocket *
 gstc_socket_new (const char *address, const unsigned int port,
     const unsigned long wait_time, const int keep_connection_open)
 {
-  return (GstcSocket *) malloc (sizeof (GstcSocket));
+  GstcSocket *self;
+  const int domain = AF_INET;
+  const int type = SOCK_STREAM;
+  const int proto = 0;
+
+  gstc_assert_and_ret_val (NULL != address, NULL);
+
+  self = (GstcSocket *) malloc (sizeof (GstcSocket));
+
+  self->socket = socket (domain, type, proto);
+  if (-1 == self->socket) {
+    goto free_self;
+  }
+
+  self->server.sin_addr.s_addr = inet_addr (address);
+  self->server.sin_family = domain;
+  self->server.sin_port = htons (port);
+
+  if (connect (self->socket, (struct sockaddr *) &self->server,
+          sizeof (self->server)) < 0) {
+    goto close_socket;
+  }
+
+  /* Success */
+  goto out;
+
+close_socket:
+  close (self->socket);
+
+free_self:
+  free (self);
+  self = NULL;
+
+out:
+  return self;
 }
 
 GstcStatus
-gstc_socket_send (GstcSocket * socket, const char *request,
-    char ** response)
+gstc_socket_send (GstcSocket * self, const char *request, char **response)
 {
+  gstc_assert_and_ret_val (NULL != self, GSTC_NULL_ARGUMENT);
+  gstc_assert_and_ret_val (NULL != request, GSTC_NULL_ARGUMENT);
+  gstc_assert_and_ret_val (NULL != response, GSTC_NULL_ARGUMENT);
+
+  if (send (self->socket, request, strlen (request), 0) < 0) {
+    return GSTC_SEND_ERROR;
+  }
+
+  *response = malloc (GSTC_MAX_RESPONSE_LENGTH);
+
+  if (recv (self->socket, *response, GSTC_MAX_RESPONSE_LENGTH, 0) < 0) {
+    return GSTC_RECV_ERROR;
+  }
+
   return GSTC_OK;
 }
 
 void
 gstc_socket_free (GstcSocket * socket)
 {
+  close (socket->socket);
   free (socket);
 }
---
+++ tests/libgstc/test_libgstc_socket.c
@@ -0,0 +1,148 @@
+/*
+ * GStreamer Daemon - Gst Launch under steroids
+ * Copyright (c) 2015-2018 Ridgerun, LLC (http://www.ridgerun.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+#include <gst/check/gstcheck.h>
+#include <gio/gio.h>
+
+#include "libgstc.h"
+#include "libgstc_socket.h"
+
+/* Mock implementation of gstd */
+GMainLoop *_loop;
+GSocketService * _mock_server;
+GThread * _mock_thread;
+const gchar *_mock_expected;
+
+static gboolean
+mock_server_cb  (GSocketService *service, GSocketConnection *connection,
+    GObject *source_object, gpointer user_data)
+{
+  GError * error;
+  GInputStream * istream = g_io_stream_get_input_stream (G_IO_STREAM (connection));
+  GOutputStream * ostream = g_io_stream_get_output_stream (G_IO_STREAM (connection));
+  gchar message[64];
+  
+  g_input_stream_read  (istream,
+			message,
+			1024,
+			NULL,
+			&error);
+  fail_if (error);
+
+  fail_if (NULL == _mock_expected);
+  g_output_stream_write(ostream,
+			_mock_expected,
+			strlen(_mock_expected)+1,
+			NULL,
+			&error);
+  fail_if (error);
+
+  return TRUE;
+}
+
+gpointer
+mock_server_thread (gpointer data)
+{
+  g_main_loop_run(_loop);
+
+  return NULL;
+}
+
+static void
+mock_server_new ()
+{
+  GError * error = NULL;
+
+  _mock_server = g_socket_service_new ();
+  _mock_expected = NULL;
+
+  g_socket_listener_add_inet_port ((GSocketListener*)_mock_server,
+     54321, NULL, &error);
+  fail_if (error);
+
+  g_signal_connect (_mock_server, "incoming", G_CALLBACK (mock_server_cb), NULL);
+  g_socket_service_start (_mock_server);
+
+  _loop = g_main_loop_new(NULL, FALSE);
+  _mock_thread = g_thread_new ("mock_server", mock_server_thread, NULL);
+  
+}
+
+static void
+mock_server_free ()
+{
+  g_socket_service_stop (_mock_server);
+  g_object_unref (_mock_server);
+  
+  g_main_loop_quit (_loop);
+  g_main_loop_unref(_loop);
+  g_thread_join (_mock_thread);
+  g_thread_unref (_mock_thread);
+}
+
+void setup ()
+{
+  mock_server_new ();
+}
+
+void teardown ()
+{
+  mock_server_free ();
+}
+
+GST_START_TEST (test_socket_success)
+{
+  GstcSocket * socket;
+  GstcStatus ret;
+  const gchar *address = "127.0.0.1";
+  const gint port = 54321;
+  const unsigned long wait_time = 0;
+  const gint keep_open = TRUE;
+  const gchar *request = "ping";
+  const gchar *expected = "pong";
+  gchar *response;
+
+  socket = gstc_socket_new(address, port, wait_time, keep_open);
+  fail_if (NULL == socket);
+
+  _mock_expected = expected;
+  ret = gstc_socket_send(socket, request, &response);
+
+  assert_equals_int (GSTC_OK, ret);
+  assert_equals_string (expected, response);
+
+  g_free (response);
+  gstc_socket_free (socket);  
+}
+GST_END_TEST;
+
+static Suite *
+libgstc_client_suite (void)
+{
+  Suite *suite = suite_create ("libgstc_socket");
+  TCase *tc = tcase_create ("general");
+
+  suite_add_tcase (suite, tc);
+
+  tcase_add_checked_fixture (tc, setup, teardown);
+  tcase_add_test (tc, test_socket_success);
+
+  return suite;
+}
+
+GST_CHECK_MAIN (libgstc_client);
