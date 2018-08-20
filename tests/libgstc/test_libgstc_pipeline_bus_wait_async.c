8ed7c9781cc0695fd42558ec1794edf995ce37b1
Emmanuel Madrigal <emmanuel.madrigal@ridgerun.com>
2018-08-20 16:30:59 -0600

    Add mutex locks to ensure correct bus wait async test

--- tests/libgstc/test_libgstc_pipeline_bus_wait_async.c
+++ tests/libgstc/test_libgstc_pipeline_bus_wait_async.c
@@ -17,7 +17,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 #include <gst/check/gstcheck.h>
+#include <pthread.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "libgstc.h"
 #include "libgstc_socket.h"
@@ -26,6 +29,8 @@
 /* Test Fixture */
 static gchar _request[3][512];
 static GstClient *_client;
+pthread_mutex_t lock;
+int socket_send_wait_time = 0;
 
 static void
 setup ()
@@ -73,9 +78,13 @@ gstc_socket_send (GstcSocket * socket, const gchar * request, gchar ** response,
 
   *response = malloc (1);
 
+  if (reqnum == 2) {
+    sleep (socket_send_wait_time);
+  }
   memcpy (_request[reqnum], request, strlen (request));
 
   reqnum++;
+
 
   return GSTC_OK;
 }
@@ -106,6 +115,8 @@ callback (GstClient * _client, const gchar * pipeline_name,
     const gchar * message_name, const long long timeout, char *message,
     gpointer user_data)
 {
+  /* Unlock the mutex */
+  pthread_mutex_unlock (&lock);
   return GSTC_OK;
 }
 
@@ -120,6 +131,21 @@ GST_START_TEST (test_pipeline_bus_wait_async_success)
     "read /pipelines/pipe/bus/message"
   };
 
+  /* Mutex timeout 10ms */
+  struct timespec ts;
+  clock_gettime (CLOCK_REALTIME, &ts);
+  ts.tv_nsec += 10 * 1000000;
+
+  if (pthread_mutex_init (&lock, NULL) != 0) {
+    /* Exit if unable to create mutex lock */
+    ASSERT_CRITICAL ();
+  }
+
+  /*
+   * Lock the mutex, this should be unlocked by the callback function
+   */
+  pthread_mutex_lock (&lock);
+
   ret =
       gstc_pipeline_bus_wait_async (_client, pipeline_name, message_name,
       timeout, callback, NULL);
@@ -127,7 +153,58 @@ GST_START_TEST (test_pipeline_bus_wait_async_success)
 
   assert_equals_string (expected[0], _request[0]);
   assert_equals_string (expected[1], _request[1]);
+
+  /* Wait for the callback function to finish or timeout passes */
+  pthread_mutex_timedlock (&lock, &ts);
   assert_equals_string (expected[2], _request[2]);
+  pthread_mutex_unlock (&lock);
+}
+
+GST_END_TEST;
+
+GST_START_TEST (test_pipeline_bus_wait_async_bus_didnt_respond)
+{
+  GstcStatus ret;
+  const gchar *pipeline_name = "pipe";
+  const gchar *message_name = "eos";
+  const gint64 timeout = -1;
+  const gchar *expected[] = { "update /pipelines/pipe/bus/types eos",
+    "update /pipelines/pipe/bus/timeout -1"
+  };
+  /* Set the send command to wait for 1s before responding */
+  socket_send_wait_time = 1;
+
+  /* Mutex timeout 10ms */
+  struct timespec ts;
+  clock_gettime (CLOCK_REALTIME, &ts);
+  ts.tv_sec += 0;
+  ts.tv_nsec += 10 * 1000000;
+
+  if (pthread_mutex_init (&lock, NULL) != 0) {
+    /* Exit if unable to create mutex lock */
+    ASSERT_CRITICAL ();
+  }
+
+  /*
+   * Lock the mutex, this should be unlocked by the callback function
+   */
+  pthread_mutex_lock (&lock);
+
+  ret =
+      gstc_pipeline_bus_wait_async (_client, pipeline_name, message_name,
+      timeout, callback, NULL);
+  assert_equals_int (GSTC_OK, ret);
+
+  assert_equals_string (expected[0], _request[0]);
+  assert_equals_string (expected[1], _request[1]);
+
+  /* Wait for the callback function to finish or timeout passes */
+  pthread_mutex_timedlock (&lock, &ts);
+  assert_equals_string ("", _request[2]);
+  pthread_mutex_unlock (&lock);
+
+  /* Reset value */
+  socket_send_wait_time = 0;
 }
 
 GST_END_TEST;
@@ -142,6 +219,7 @@ libgstc_pipeline_bus_wait_async_suite (void)
 
   tcase_add_checked_fixture (tc, setup, teardown);
   tcase_add_test (tc, test_pipeline_bus_wait_async_success);
+  tcase_add_test (tc, test_pipeline_bus_wait_async_bus_didnt_respond);
 
   return suite;
 }
