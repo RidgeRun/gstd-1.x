0f5e79433007d50828dcf924b0d4e8b1a0404ea6
Miguel Taylor <miguel.taylor@ridgerun.com>
2019-08-13 10:29:47 -0600

    Add tests for create

---
+++ tests/libgstc/python/test_libgstc_python_create.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import unittest
+import gstc
+import os
+
+class TestGstcCreateMethods(unittest.TestCase):
+    def setUp(self):
+        self.gstd_client = gstc.client(loglevel='DEBUG')
+
+    def test_create_pipeline(self):
+        pipeline = "videotestsrc name=v0 ! xvimagesink"
+        ret = self.gstd_client.read("pipelines")
+        initial_n_pipes = len(ret['response']['nodes'])
+        self.assertEqual(self.gstd_client.create ("pipelines", "p0", pipeline), 0)
+        ret = self.gstd_client.read("pipelines")
+        final_n_pipes = len(ret['response']['nodes'])
+        self.assertEqual(final_n_pipes, initial_n_pipes+1)
+if __name__ == '__main__':
+    unittest.main()
