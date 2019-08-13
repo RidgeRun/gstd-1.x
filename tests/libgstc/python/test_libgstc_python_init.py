d60ffcc81178870e8f941bb90b737592e6f00eb4
Miguel Taylor <miguel.taylor@ridgerun.com>
2019-08-13 10:00:49 -0600

    Add init tests

---
+++ tests/libgstc/python/test_libgstc_python_init.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import unittest
+import gstc
+import os
+
+class TestGstcInitMethods(unittest.TestCase):
+    def test_init(self):
+        self.gstd_client = gstc.client()
+
+    def test_init_loglevel(self):
+        self.gstd_client = gstc.client(loglevel='DEBUG')
+
+    def test_init_logfile(self):
+        f = open("dummy.log","w+")
+        f.close()
+        f = open('dummy.log')
+        num_lines_init = sum(1 for line in f)
+        f.close()
+        self.gstd_client = gstc.client(logfile='dummy.log', loglevel='DEBUG')
+        f = open('dummy.log')
+        num_lines_final = sum(1 for line in f)
+        f.close()
+        self.assertNotEqual(num_lines_init, num_lines_final)
+        os.remove("dummy.log")
+
+if __name__ == '__main__':
+    unittest.main()
