8229b19bdf826335635daa8d60aa085c456233a7
Miguel Taylor <miguel.taylor@ridgerun.com>
2019-08-13 11:11:55 -0600

    Add test for delete

---
+++ tests/libgstc/python/test_libgstc_python_delete.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import unittest
+import gstc
+
+class TestGstcDeleteMethods(unittest.TestCase):
+    def setUp(self):
+        self.gstd_client = gstc.client(loglevel='DEBUG')
+
+    def test_delete_pipeline(self):
+        pipeline = "videotestsrc name=v0 ! xvimagesink"
+        ret = self.gstd_client.read("pipelines")
+        initial_n_pipes = len(ret['response']['nodes'])
+        self.assertEqual(self.gstd_client.create ("pipelines", "p0", pipeline), 0)
+        ret = self.gstd_client.read("pipelines")
+        final_n_pipes = len(ret['response']['nodes'])
+        self.assertEqual(final_n_pipes, initial_n_pipes+1)
+        self.assertEqual(self.gstd_client.delete ("pipelines", "p0"), 0)
+        ret = self.gstd_client.read("pipelines")
+        final_n_pipes = len(ret['response']['nodes'])
+        self.assertEqual(final_n_pipes, initial_n_pipes)
+if __name__ == '__main__':
+    unittest.main()
