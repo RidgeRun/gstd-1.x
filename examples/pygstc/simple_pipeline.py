1c726209a2dacc68177af49287d8af6a1f4f752d
Abraham Arias <abraham.arias@ridgerun.com>
2020-04-23 08:50:17 -0600

    Handle GstdError and GsrcError

--- examples/pygstc/simple_pipeline.py
+++ examples/pygstc/simple_pipeline.py
@@ -29,60 +29,69 @@ try:
 except:
   print("Error: The GstdClient could not be created")
 else:
-  if(len(sys.argv) > 1):
-    if(sys.argv[1]=="create"):
+  try:
+    if(len(sys.argv) > 1):
+      status = 0
+      if(sys.argv[1]=="create"):
 
-      FILE_SOURCE = sys.argv[2]
-      #pipeline is the string with the pipeline description
-      pipeline = "filesrc location="+FILE_SOURCE+" ! decodebin ! videoconvert ! \
-        videoscale ! capsfilter name=cf ! autovideosink"
+        FILE_SOURCE = sys.argv[2]
+        #pipeline is the string with the pipeline description
+        pipeline = "filesrc location="+FILE_SOURCE+" ! decodebin ! videoconvert ! \
+          videoscale ! capsfilter name=cf ! autovideosink"
 
-      #Following instructions create and play the pipeline
-      gstc.pipeline_create("p0", pipeline)
+        #Following instructions create and play the pipeline
+        gstc.pipeline_create("p0", pipeline)
 
-    elif(sys.argv[1]== "play"):
-      gstc.pipeline_play("p0")
-      print("Playing")
+      elif(sys.argv[1]== "play"):
+        gstc.pipeline_play("p0")
+        print("Playing")
 
-    #Reverse and slow motion restart the pipeline
-    elif(sys.argv[1]== "reverse"):
-      gstc.event_seek("p0", rate=-1.0, format=3, flags=1, start_type=1, start=0, end_type=1, end=-1)
-      print("Playing in reverse")
+      #Reverse and slow motion restart the pipeline
+      elif(sys.argv[1]== "reverse"):
+        gstc.event_seek("p0", rate=-1.0, format=3, flags=1, start_type=1, start=0, end_type=1, end=-1)
+        print("Playing in reverse")
 
-    elif(sys.argv[1]== "slow_motion"):
-      gstc.event_seek("p0", rate=0.5, format=3, flags=1, start_type=1, start=0, end_type=1, end=-1)
-      print("Playing in slow motion")
+      elif(sys.argv[1]== "slow_motion"):
+        gstc.event_seek("p0", rate=0.5, format=3, flags=1, start_type=1, start=0, end_type=1, end=-1)
+        print("Playing in slow motion")
 
-    elif(sys.argv[1]== "pause"):
-      gstc.pipeline_pause("p0")
-      print("Pipeline paused")
+      elif(sys.argv[1]== "pause"):
+        gstc.pipeline_pause("p0")
+        print("Pipeline paused")
 
-    elif(sys.argv[1]== "stop"):
-      gstc.pipeline_stop("p0")
-      print("Pipeline stoped")
+      elif(sys.argv[1]== "stop"):
+        gstc.pipeline_stop("p0")
+        print("Pipeline stoped")
 
-    elif(sys.argv[1]== "delete"):
-      gstc.pipeline_delete ("p0")
-      print("Pipeline deleted")
+      elif(sys.argv[1]== "delete"):
+        gstc.pipeline_delete ("p0")
+        print("Pipeline deleted")
 
-    elif(sys.argv[1] == "read_gstd"):
-      #Timeout in nanoseconds or forever
-      gstc.bus_timeout("p0", -1)
-      resp = gstc.bus_read("p0")
-      print(resp)
+      elif(sys.argv[1] == "read_gstd"):
+        #Timeout in nanoseconds or forever
+        gstc.bus_timeout("p0", -1)
+        resp = gstc.bus_read("p0")
+        print(resp)
 
-    elif(sys.argv[1] == "read_f_gstd"):
-      #Serach EOF and react
-      gstc.bus_filter("p0", "error+eos")
-      resp = gstc.bus_read("p0")
-      print(resp)
+      elif(sys.argv[1] == "read_f_gstd"):
+        #Serach EOF and react
+        gstc.bus_filter("p0", "error+eos")
+        resp = gstc.bus_read("p0")
+        print(resp)
 
-    elif(sys.argv[1] == "set_res" and len(sys.argv) == 4):
-      width = sys.argv[2]
-      height = sys.argv[3]
-      gstc.element_set("p0", "cf", "caps", "video/x-raw,width="+width+",height="+height+"")
+      elif(sys.argv[1] == "set_res" and len(sys.argv) == 4):
+        width = sys.argv[2]
+        height = sys.argv[3]
+        gstc.element_set("p0", "cf", "caps", "video/x-raw,width="+width+",height="+height+"")
 
+      else:
+        status = -1
     else:
-      printError()
-  else:
-      printError()
+        status = -1
+  except GstdError:
+    print("GstdError: Gstd IPC failed")
+  except GstcError:
+    print("GstcError: Gstd python client failed")
+
+if (status == -1):
+  printError()
