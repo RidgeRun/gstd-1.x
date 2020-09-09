46cd099ae31e576b33e7f904076d7f1bf687b1e5
Abraham Arias <abraham.arias@ridgerun.com>
2020-09-09 08:59:18 -0600

    Remove mixed declarations from code

--- gst_client/gst_client.c
+++ gst_client/gst_client.c
@@ -333,6 +333,8 @@ main (gint argc, gchar * argv[])
   };
   quark = g_quark_from_static_string (GSTD_CLIENT_DOMAIN);
 
+  quark = g_quark_from_static_string (GSTD_CLIENT_DOMAIN);
+
   /* Internationalization */
   setlocale (LC_ALL, "");
 
