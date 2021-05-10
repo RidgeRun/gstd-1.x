4faa2760aa4f4d2b5abf491ebdefd49a3c0bee52
Miguel Taylor <miguel.taylor@ridgerun.com>
2021-05-11 07:19:14 +0800

    Update copyright headers

--- gstd/gstd_state.h
+++ gstd/gstd_state.h
@@ -1,6 +1,6 @@
 /*
  * Gstreamer Daemon - Gst Launch under steroids
- * Copyright (C) 2017 RidgeRun Engineering <support@ridgerun.com>
+ * Copyright (C) 2017-2021 RidgeRun Engineering <support@ridgerun.com>
  *
  * This file is part of Gstd.
  *
