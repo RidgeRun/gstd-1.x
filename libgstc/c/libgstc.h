6edf729501ec56f514a4e02ae9ed2b21de99d88a
Michael Gruner <michael.gruner@ridgerun.com>
2022-08-05 16:39:04 -0600

    Standarize all client bindings to BSD-3-clause

--- libgstc/c/libgstc.h
+++ libgstc/c/libgstc.h
@@ -3,8 +3,8 @@
  * Copyright 2015-2022 Ridgerun, LLC (http://www.ridgerun.com)
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
